14a5fa2345cf5b1acfd1621342077b4afbd14264
Julien Hilaire <julien.hilaire@imelmi.net>
2024-02-10 09:26:23 +0100

    amélioration gestion des balises latex

--- 12679-boutons.js
+++ 12679-boutons.js
@@ -13,72 +13,89 @@ $(function () {
         };
 
 
+        function setLatexTag() {
+            // Récupérer la sélection dans l'éditeur de texte SCEditor
+            var selection = editoR.getRangeHelper().selectedHtml();
+
+            // Vérifier si du texte est sélectionné
+            if (selection) {
+                // Encadrer le texte sélectionné avec des guillemets
+                var texteEncadre = "[latex]" + selection + "[/latex]";
+
+                // Insérer le texte encadré dans l'éditeur de texte SCEditor
+                editoR.insert(texteEncadre);
+            } else {
+                editoR.insert('[latex][/latex]');
+            }
+        }
+
+
         // Définition des boutons à ajouter
         var buttons = [{
-            img: "https://i.ibb.co/txkQt72/Calculette.png",
-            title: "Equation",
-            action: mathsButtonsToogleView
-        },
-            {
-                img: "https://i.ibb.co/GTkBZ4Y/lx.png",
-                title: "Latex",
-                action: function () {
-                    editoR.insert('[latex][/latex]');
-                }
+                img: "https://i.ibb.co/txkQt72/Calculette.png",
+                title: "Equation",
+                action: mathsButtonsToogleView
             },
-            {
-                img: "https://i.ibb.co/YX1nMm6/fraction2.png",
-                title: "Fraction",
-                action: function () {
-                    editoR.insert('\\frac{a}{b}');
-                }
-            },
+                {
+                    //img: "https://i.ibb.co/GTkBZ4Y/lx.png",
+                    img:"https://i.ibb.co/b72Vkmm/lxred.png",
+                    title: "Latex",
+                    action: setLatexTag
+                },
+                {
+                    img: "https://i.ibb.co/YX1nMm6/fraction2.png",
+                    title: "Fraction",
+                    action: function () {
+                        editoR.insert('\\frac{a}{b}');
+                    }
+                },
 
-            {
-                img: "https://i.ibb.co/Wvdx00w/add.png",
-                title: "Add",
-                action: function () {
-                    editoR.insert('+');
-                }
-            },
-            {
-                img: "https://i.ibb.co/3zQ6ztC/equal.png",
-                title: "Equal",
-                action: function () {
-                    editoR.insert('=');
-                }
-            },
+                {
+                    img: "https://i.ibb.co/Wvdx00w/add.png",
+                    title: "Add",
+                    action: function () {
+                        editoR.insert('+');
+                    }
+                },
+                {
+                    img: "https://i.ibb.co/3zQ6ztC/equal.png",
+                    title: "Equal",
+                    action: function () {
+                        editoR.insert('=');
+                    }
+                },
 
-            {
-                img: "https://i.ibb.co/Zx6yW7g/greater-than.png",
-                title: "Greater than",
-                action: function () {
-                    editoR.insert('>');
-                }
-            },
-            {
-                img: "https://i.ibb.co/Zx6yW7g/greater-than.png",
-                title: "Less than",
-                action: function () {
-                    editoR.insert('<');
-                }
-            },
+                {
+                    img: "https://i.ibb.co/Zx6yW7g/greater-than.png",
+                    title: "Greater than",
+                    action: function () {
+                        editoR.insert('>');
+                    }
+                },
+                {
+                    img: "https://i.ibb.co/Zx6yW7g/greater-than.png",
+                    title: "Less than",
+                    action: function () {
+                        editoR.insert('<');
+                    }
+                },
 
-            {
-                img: "https://i.ibb.co/vPcxH54/pi.png",
-                title: "Pi",
-                action: function () {
-                    editoR.insert('\\pi');
+                {
+                    img: "https://i.ibb.co/vPcxH54/pi.png",
+                    title: "Pi",
+                    action: function () {
+                        editoR.insert('\\pi');
+                    }
+                },
+                {
+                    img: "https://i.ibb.co/QMS34Pk/root.png",
+                    title: "Root",
+                    action: function () {
+                        editoR.insert('\\sqrt{a}');
+                    }
                 }
-            },
-            {
-                img: "https://i.ibb.co/QMS34Pk/root.png",
-                title: "Root",
-                action: function () {
-                    editoR.insert('\\sqrt{a}');
-                }
-            }
-        ];
+            ]
+        ;
 
         $('<div class="sceditor-group before"></div>').insertBefore('.sceditor-group:first');
         // Boucle sur le tableau d'objets pour générer les boutons dynamiquement
@@ -88,4 +105,5 @@ $(function () {
             $('.sceditor-' + (index + 1)).click(button.action);
         });
         mathsButtonsToogleView(); // Appel initial pour configurer la visibilité
-    });})
+    });
+})
